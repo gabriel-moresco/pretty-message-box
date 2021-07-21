d075075480b44861c7dd80801e75ba5dd1847826
Gabriel Moresco <gabriel.moresco25@gmail.com>
2021-07-21 16:48:39 -0300

    Added project to test the nuget package

---
+++ PrettyMessageBox.Test.Nuget/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrettyMessageBox.Test.Nuget
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            MessageBox.Show("Teste", "Teste");
+            MessageBox.Show("Teste", "Teste", ImageOptions.Checked);
+            MessageBox.Show("Teste", "Teste", ButtonOptions.Ok);
+            MessageBox.Show("Teste", "Teste", ButtonOptions.OkAuxiliary, "Teste");
+            MessageBox.Show("Teste", "Teste", ImageOptions.Error, ButtonOptions.OkAuxiliary, "Teste");
+            MessageBox.Show("Teste", "Teste", ImageOptions.Error, ButtonOptions.OkCancel);
+            MessageBox.Show("Teste", "Teste", ImageOptions.Error, ButtonOptions.YesNoAuxiliary, "Teste");
+        }
+    }
+}
