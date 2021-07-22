3319e36246b57ed214f7fa61060f782379e5de13
Gabriel Moresco <gabriel.moresco25@gmail.com>
2021-07-22 08:00:59 -0300

    Added sample images

--- PrettyMessageBox.Test.Nuget/Program.cs
+++ PrettyMessageBox.Test.Nuget/Program.cs
@@ -7,13 +7,28 @@ namespace PrettyMessageBox.Test.Nuget
         [STAThread]
         static void Main(string[] args)
         {
-            MessageBox.Show("Teste", "Teste");
-            MessageBox.Show("Teste", "Teste", ImageOptions.Checked);
-            MessageBox.Show("Teste", "Teste", ButtonOptions.Ok);
-            MessageBox.Show("Teste", "Teste", ButtonOptions.OkAuxiliary, "Teste");
-            MessageBox.Show("Teste", "Teste", ImageOptions.Error, ButtonOptions.OkAuxiliary, "Teste");
-            MessageBox.Show("Teste", "Teste", ImageOptions.Error, ButtonOptions.OkCancel);
-            MessageBox.Show("Teste", "Teste", ImageOptions.Error, ButtonOptions.YesNoAuxiliary, "Teste");
+            TestMessageBox();
+        }
+
+        static void TestMessageBox()
+        {
+            MessageBox.Show("Title", "Test Content");
+            MessageBox.Show("Title", "Test Content", ImageOptions.Checked);
+            MessageBox.Show("Title", "Test Content", ButtonOptions.Ok);
+            MessageBox.Show("Title", "Test Content", ButtonOptions.OkAuxiliary, "Test Aux");
+            MessageBox.Show("Title", "Test Content", ImageOptions.Info, ButtonOptions.OkAuxiliary, "Test Aux");
+            MessageBox.Show("Title", "Test Content", ImageOptions.Info, ButtonOptions.OkCancel);
+            MessageBox.Show("Title", "Test Content", ImageOptions.Question, ButtonOptions.YesNoAuxiliary, "Test Aux");
+
+            try
+            {
+                int.Parse("");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Title", ex);
+                MessageBox.Show("Title", "An error occurred:", ex);
+            }
         }
     }
 }
