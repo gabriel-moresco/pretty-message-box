08fba8769f6541036e5eba1aee479f23e79aaffe
Gabriel Moresco <gabriel.moresco25@gmail.com>
2021-07-23 14:48:16 -0300

    Published nuget package version 1.1.1

--- PrettyMessageBox/Properties/AssemblyInfo.cs
+++ PrettyMessageBox/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PrettyMessageBox")]
-[assembly: AssemblyDescription("A pretty Message Box to use in your WPF projects")]
+[assembly: AssemblyDescription("A PrettyMessageBox to use in your WPF projects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gabriel Moresco")]
 [assembly: AssemblyProduct("PrettyMessageBox")]
@@ -47,5 +47,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
